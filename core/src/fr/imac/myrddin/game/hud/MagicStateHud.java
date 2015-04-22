a3aec2f8f18d73c2af058d8ebcbdcd5a12669409
mxmgilbert <mxm.gilbert@gmail.com>
2015-04-22 12:32:18 +0200

    Update Magic State position and remove label

--- core/src/fr/imac/myrddin/game/hud/MagicStateHud.java
+++ core/src/fr/imac/myrddin/game/hud/MagicStateHud.java
@@ -12,14 +12,14 @@ import fr.imac.myrddin.game.myrddin.Myrddin;
 
 public class MagicStateHud extends Actor {
 	private Myrddin myrddin;
-	private BitmapFont bitmapfont;
+	//private BitmapFont bitmapfont;
 	private AtlasRegion atlasRegionPower1;
 	private AtlasRegion atlasRegionPower2;
 	
 	public MagicStateHud (Myrddin myrddin) {
 		this.myrddin = myrddin;
 		
-		bitmapfont = (BitmapFont) MyrddinGame.assetManager.get("ui/test.fnt", BitmapFont.class);
+		//bitmapfont = (BitmapFont) MyrddinGame.assetManager.get("ui/test.fnt", BitmapFont.class);
 		
 		TextureAtlas textureAtlas = MyrddinGame.assetManager.get("ui/hud.atlas", TextureAtlas.class);
 		atlasRegionPower1 = textureAtlas.findRegion("magicFire");
@@ -31,26 +31,26 @@ public class MagicStateHud extends Actor {
 		if (myrddin.getMagicState() == MagicState.POWER_1) {
 			batch.draw(
 					atlasRegionPower1,
-					getParent().getWidth() - atlasRegionPower1.getRegionWidth() - 20,
-					getParent().getHeight() - atlasRegionPower1.getRegionHeight() - 55
+					getParent().getWidth() - atlasRegionPower1.getRegionWidth() - 120,
+					getParent().getHeight() - atlasRegionPower1.getRegionHeight() - 5
 			);
 		}
 		else {
 			batch.draw(
 					atlasRegionPower2,
-					getParent().getWidth() - atlasRegionPower2.getRegionWidth() - 20,
-					getParent().getHeight() - atlasRegionPower2.getRegionHeight() - 55
+					getParent().getWidth() - atlasRegionPower2.getRegionWidth() - 120,
+					getParent().getHeight() - atlasRegionPower2.getRegionHeight() - 5
 			);
 		}
 		
 		
-		bitmapfont.setColor(1f, 1f, 1f, 1f);
-		bitmapfont.setScale(0.5f);
-		bitmapfont.draw(
-				batch,
-				"pouvoir : ",
-				getParent().getWidth() - 150,
-				getParent().getHeight() - 55
-		);
+		//bitmapfont.setColor(1f, 1f, 1f, 1f);
+		//bitmapfont.setScale(0.5f);
+		//bitmapfont.draw(
+		//		batch,
+		//		"pouvoir : ",
+		//		getParent().getWidth() - 150,
+		//		getParent().getHeight() - 55
+		//);
 	}
 }
